d4cb6c9c79cf0bee8f43be81758b93bfd4861409
Andrew <huli-gan_94@mail.ru>
2025-03-08 20:58:06 +0300

    Добавил next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Включает static export
+    trailingSlash: true, // Добавляет / в конце ссылок (например, /about/)
+  };
+  
+  module.exports = nextConfig;
+  
